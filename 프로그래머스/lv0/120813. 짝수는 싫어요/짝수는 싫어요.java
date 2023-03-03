f8b3d1319e8c46127704e299ba73f5d22f48c12b
Grey Kim <104502156+greyco1or@users.noreply.github.com>
2023-03-03 11:58:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -2,7 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[n/2 + 1];
+        int cnt = 0;
+        if(n%2 == 0) {
+            cnt = n/2;
+        } else {
+            cnt = n/2+1;
+        }
+        int[] answer = new int[cnt];
         int j = 0;
         for(int i = 1; i <= n; i++) {
             if(i%2 == 1) {
@@ -11,13 +17,6 @@ class Solution {
             }
         }
         
-        if(answer[answer.length-1] == 0) {
-            int[] anotherAnswer = new int[answer.length-1];
-            for(int i = 0; i < answer.length-1; i++) {
-                anotherAnswer[i] = answer[i];
-            }
-            return anotherAnswer;
-        }
         return answer;
     }
 }
