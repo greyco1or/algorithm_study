23dbbf8ea47f85bebb99115f2a9f1bf9f54634fc
Grey Kim <104502156+greyco1or@users.noreply.github.com>
2023-03-09 10:54:14 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,19 +1,16 @@
 class Solution {
     public int solution(int price) {
         int answer = 0;
-        if(price >= 100000) {
-            if(price < 300000) {
-                answer = price*95/100;
-            } else {
-                if(price < 500000) {
-                    answer = price*90/100;
-                } else {
-                    answer = price*80/100;
-                }
-            }
-        } else {
-            answer = price;
+        if(price >= 500000) {
+            return price*80/100;
         }
+        if(price >= 300000) {
+            return price*90/100;
+        }
+        if(price >= 100000) {
+            return price*95/100;
+        }
+        answer = price;
         return answer;
     }
 }
