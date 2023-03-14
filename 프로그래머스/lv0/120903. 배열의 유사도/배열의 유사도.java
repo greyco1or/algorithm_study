dac6d8a0c9346e73f2e8a52c14379d600234c00a
Grey Kim <104502156+greyco1or@users.noreply.github.com>
2023-03-14 10:15:40 +0900

    [level 0] Title: 배열의 유사도, Time: 0.09 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,11 +1,12 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
+        HashSet<String> set = new HashSet<String>(List.of(s2));
         for(String i : s1) {
-            for(String j : s2) {
-                if(i.equals(j)) {
-                    answer++;
-                }
+            if(set.contains(i)) {
+                answer++;
             }
         }
         
